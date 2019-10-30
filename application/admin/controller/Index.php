7e286ab33e3ee4f4381f3e9210437073ef080c65
wmc1125 <287851074@qq.com>
2019-10-30 16:25:18 +0800

    后台首页显示信息

--- application/admin/controller/Index.php
+++ application/admin/controller/Index.php
@@ -25,36 +25,49 @@ class Index extends Admin
     {
         admin_role_check($this->z_role_list,$this->mca);
         //  用户增长曲线
-        $user_nyr_grow = Db::name('user')
-                    ->field("DATE_FORMAT(FROM_UNIXTIME(ctime),'%Y-%m-%d') as date, count(DATE_FORMAT(FROM_UNIXTIME(ctime),'%Y-%m-%d')) as sum")
-                    ->group("DATE_FORMAT(FROM_UNIXTIME(ctime),'%Y-%m-%d')")
-                    ->select();
-        foreach($user_nyr_grow as $k=>$vo){
-            $user_nyr_grow_chart['nyr'][$k] = $vo['date'];
-            $user_nyr_grow_chart['sum'][$k] = $vo['sum'];
-        }
-        $this->assign('user_nyr_grow_chart',$user_nyr_grow_chart);
-        $this->assign('user_nyr_grow',$user_nyr_grow);
+        // $user_nyr_grow = Db::name('user')
+        //             ->field("DATE_FORMAT(FROM_UNIXTIME(ctime),'%Y-%m-%d') as date, count(DATE_FORMAT(FROM_UNIXTIME(ctime),'%Y-%m-%d')) as sum")
+        //             ->group("DATE_FORMAT(FROM_UNIXTIME(ctime),'%Y-%m-%d')")
+        //             ->select();
+        // foreach($user_nyr_grow as $k=>$vo){
+        //     $user_nyr_grow_chart['nyr'][$k] = $vo['date'];
+        //     $user_nyr_grow_chart['sum'][$k] = $vo['sum'];
+        // }
+        // $this->assign('user_nyr_grow_chart',$user_nyr_grow_chart);
+        // $this->assign('user_nyr_grow',$user_nyr_grow);
 
-        //性别比例
-        $user_sex = Db::name('user')
-                    ->field("sex, count(sex) as sum")
-                    ->group("sex")
-                    ->select();
-        foreach($user_sex as $k=>$vo){
-            // 0	未知	
-            // 1	男性	
-            // 2	女性
-            if($vo['sex']==1){
-                $user_sex_chart[$k]['sex'] = '男';
-            }elseif($vo['sex']==2){
-                $user_sex_chart[$k]['sex'] = '女';
-            }else{
-                $user_sex_chart[$k]['sex'] = '未知';
-            }
-            $user_sex_chart[$k]['sum'] = $vo['sum'];
-        }
-        $this->assign('user_sex_chart',$user_sex_chart);
+        // //性别比例
+        // $user_sex = Db::name('user')
+        //             ->field("sex, count(sex) as sum")
+        //             ->group("sex")
+        //             ->select();
+        // foreach($user_sex as $k=>$vo){
+        //     // 0	未知	
+        //     // 1	男性	
+        //     // 2	女性
+        //     if($vo['sex']==1){
+        //         $user_sex_chart[$k]['sex'] = '男';
+        //     }elseif($vo['sex']==2){
+        //         $user_sex_chart[$k]['sex'] = '女';
+        //     }else{
+        //         $user_sex_chart[$k]['sex'] = '未知';
+        //     }
+        //     $user_sex_chart[$k]['sum'] = $vo['sum'];
+        // }
+        // $this->assign('user_sex_chart',$user_sex_chart);
+        //用户总数
+        $data['user_total'] = Db::name('user')->where([['status','<>','9']])->count();
+        //本周用户数 
+        $data['user_week'] = Db::name('user')->where([['status','<>','9']])->whereTime('ctime','week')->count();
+        //内容总数
+        $data['post_total'] = Db::name('post')->where([['status','<>','9']])->count();
+        //本周内容
+        $data['post_week'] = Db::name('post')->where([['status','<>','9']])->whereTime('ctime','week')->count();
+        //留言数
+        $data['guessbook_total'] = Db::name('guessbook')->where([['status','<>','9']])->count();
+        //本周留言数
+        $data['guessbook_week'] = Db::name('guessbook')->where([['status','<>','9']])->whereTime('ctime','week')->count();
+        $this->assign('data',$data);
         return view();
     }
 
