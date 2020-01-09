e8503da09d9323bf2bd7c52db59db69d23d527ff
子枫 <287851074@qq.com>
2020-01-09 19:18:08 +0800

    ..
    Signed-off-by: 子枫 <287851074@qq.com>

---
+++ addons/test/controller/Index.php
@@ -0,0 +1,16 @@
+<?php
+namespace addons\test\controller;
+
+class Index
+{
+    public function index(){
+        return view();
+    }
+    public function link()
+    {
+        echo 'hello link';
+//        return view();
+    }
+
+
+}
